cf3b44c5cf0e285087dbae893a49ad6789a71e0f
PBB-as <santiagopardal@outlook.com>
2019-12-23 21:08:51 -0300

    Testing.

--- app/src/main/java/com/example/cripto_photoaffix/Activities/GalleryActivity.java
+++ app/src/main/java/com/example/cripto_photoaffix/Activities/GalleryActivity.java
@@ -1,16 +1,15 @@
 package com.example.cripto_photoaffix.Activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
 import com.example.cripto_photoaffix.Visitors.Visitor;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-
+import androidx.gridlayout.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import com.example.cripto_photoaffix.R;
 
 public class GalleryActivity extends MyActivity {
@@ -30,6 +29,22 @@ public class GalleryActivity extends MyActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initialize();
+    }
+
+    private void initialize() {
+        GridLayout gridLayout = findViewById(R.id.grid_layout);
+        gridLayout.setColumnCount(3);
+        gridLayout.setRowCount(40);
+
+        for (int i = 0; i < 100; i++) {
+            ImageButton button = new ImageButton(this);
+            button.setImageResource(R.drawable.pic);
+            button.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            button.setBackgroundColor(Color.WHITE);
+            gridLayout.addView(button, 500, 500);
+        }
     }
 
     public void accept(Visitor visitor) {}
